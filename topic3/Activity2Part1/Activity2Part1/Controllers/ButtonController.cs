8a2a0807bcb2be29019a4f1c05ee9939f1607846
Brydon Johnson <brykeith@gmail.com>
2021-01-24 21:19:32 -0700

    added topic 3 assignment

---
+++ topic3/Activity2Part1/Activity2Part1/Controllers/ButtonController.cs
@@ -0,0 +1,38 @@
+﻿using Activity2Part1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity2Part1.Controllers
+{
+    public class ButtonController : Controller
+    {
+
+        public static List<ButtonModel> buttons = new List<ButtonModel>
+        {
+            new ButtonModel(state: true),
+            new ButtonModel(state: false)
+        };
+
+        // GET: Button
+        public ActionResult Index()
+        {
+            return View("Button", buttons);
+        }
+
+        public ActionResult OnButtonClick(string mine)
+        {
+            if (mine.Equals("1"))
+            {
+                buttons[0].State = !buttons[0].State;
+            }
+            if (mine.Equals("2"))
+            {
+                buttons[1].State = !buttons[1].State;
+            }
+            return View("Button", buttons);
+        }
+    }
+}
---
+++ topic3/Activity2Part1/Activity2Part1/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Activity2Part1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity2Part1.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        [Route("test")]
+        public ActionResult Index()
+        {
+            List<UserModel> UserModels = new List<UserModel>
+            {
+                new UserModel("Brad", "sports@football.com", "1234567890"),
+                new UserModel ("Ryan", "fun@ball.com", "0987654321"),
+                new UserModel ("Chelsey", "dog@cat.com", "1234567890")
+            };
+
+            return View("test", UserModels);
+        }
+    }
+}
